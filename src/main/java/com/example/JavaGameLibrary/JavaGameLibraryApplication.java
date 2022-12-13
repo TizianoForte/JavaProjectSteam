e67dba2c501b339097df7682bb21396adb4a9f19
Tiziano <forte.tiziano@gmail.com>
2022-12-13 16:30:26 +0100

    first

---
+++ src/main/java/com/example/JavaGameLibrary/JavaGameLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaGameLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGameLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGameLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaGameLibrary/JavaGameLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaGameLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGameLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
