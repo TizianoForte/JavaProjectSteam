017f8e3ef26fdb8fb6c208b26b28066fd12a75f2
genixer <davide.marin588@gmail.com>
2022-12-13 17:43:38 +0100

    Fix bug SprigBootApplication

--- src/main/java/com/example/JavaGameLibrary/JavaGameLibraryApplication.java
+++ src/main/java/com/example/JavaGameLibrary/JavaGameLibraryApplication.java
@@ -2,8 +2,9 @@ package com.example.JavaGameLibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class JavaGameLibraryApplication {
 
 	public static void main(String[] args) {
